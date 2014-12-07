0fc056a8b0a56ecbb7ae2f3bf5159a821fc77b01
sjaakp <sjaak@sjaakpriester.nl>
2014-12-07 14:25:35 +0100

    First upload

--- README.md
+++ README.md
@@ -1,4 +1,188 @@
-yii2-timeline
+YII2 Timeline
 =============
 
-Simile Timeline widget for Yii 2.0 PHP-framework
+#### Widget integrating Simile Timeline in Yii 2.0 PHP Framework. ####
+
+Timeline widget renders a Javascript Simile Timeline,  version 2.3.1. The Event data for the timeline are provided by a Yii DataProvider (any object implementing [yii\data\DataProviderInterface](http://www.yiiframework.com/doc-2.0/yii-data-dataproviderinterface.html)).
+
+A demonstration of Timeline widget is here.
+
+A little more about the Simile Timeline plus some demo's can be found here: [http://www.simile-widgets.org/timeline/](http://www.simile-widgets.org/timeline/). **Caution:** the information is very sketchy, and often contradictory or plainly wrong. Simile Timeline's code is more than eight years old and seems to be abandoned for a long time. It isn't even completed: lots and lots of more or less documented features simply are unimplemented. Nevertheless, the Simile Timeline is a great concept and the core code appears to be running quite well. **Timeline** widget only uses the well proven parts of Simile Timeline and doesn't touch the many loose ends.
+
+## Installation ##
+
+The preferred way to install **Timeline** is through [Composer](https://getcomposer.org/). Either add the following to the require section of your `composer.json` file:
+
+`"sjaakp/yii2-timeline": "*"` 
+
+Or run:
+
+`$ php composer.phar require sjaakp/yii2-timeline "*"` 
+
+You can manually install **Timeline** by [downloading the source in ZIP-format](https://github.com/sjaakp/yii2-timeline/archive/master.zip).
+
+## Using Timeline ##
+
+The code to use **Timeline** in a View is something like this:
+
+    ... other View code ...
+	<?php
+		// define Timeline
+		$t = Timeline::begin([
+			'dataProvider' => $provider,
+			'attributes' => [
+				'start' => 'startDate',
+				... more attributes ...
+			]
+			... more Timeline options ...
+		]);
+
+		// define main Band
+		$t->band([
+			'width' => '60%',
+			'intervalUnit' => Timeline::MONTH,
+			'intervalPixels' => 100
+			// layout not set, use default
+		]);
+
+		// define secundary Band
+		$t->band([
+			'width' => '40%',
+			'intervalUnit' => Timeline::YEAR,
+			'intervalPixels' => 120,
+			'layout' => 'overview'
+		]);
+
+		// complete definition
+		Timeline::end();
+	?>
+	... more View code ...
+
+The method `band()` is chainable, so this can also be written like:
+
+    <?php
+		Timeline::begin([
+			'dataProvider' => $provider,
+			'attributes' => [
+				'start' => 'startDate',
+				... more attributes ...
+			]
+			... more Timeline options ...
+		])->band([
+			'width' => '60%',
+			'intervalUnit' => Timeline::MONTH,
+			'intervalPixels' => 100
+		])->band([
+			'width' => '40%',
+			'intervalUnit' => Timeline::YEAR,
+			'intervalPixels' => 120,
+			'layout' => 'overview'
+		])->end();
+	?>
+
+#### options ####
+
+Timeline has the following options:
+
+- **dataProvider**: the DataProvider for Timeline. Must be set.
+- **attributes**: array with key => value pairs of {timeline attribute name} => {model attribute name}. This is used to 'translate' the model attribute names to Timeline attribute names. Required.
+- **height**: height of Timeline. Default: 200. Can have these values:
+ - `integer` height in pixels
+ - `string` valid CSS height (f.i. in ems)
+ - `false` height is not set; caution: the height MUST be set by some other means (CSS), otherwise Timeline will not appear.
+- **start** (optional): start date
+- **end** (optional): end date
+- **center** (optional): initial position of Timeline
+- **htmlOptions** (optional): array of HTML options for the Timeline container. Use this if you want to explicitly set the ID. 
+
+## Bands ##
+
+**Timeline** consists of one or more Bands. They each display the Events in a different time resolution.
+
+A Band is defined by the Timeline method `band()`.
+
+    public function band( $options, $zones = null )
+
+#### options ####
+
+`$options` is an array with the following keys:
+
+- **width**: the part of Timeline occupied by this band, as a percentage or another CSS3 dimension (yes, you're right: 'height' would be a more proper name, but this is how Simile Timiline defines it),
+- **layout**: the only sensible value is 'overview'; 'detailed' will also work, but seems to be very buggy; all other values (including none) default to 'compact', which is the layout of the main band
+- **intervalUnit**: the time unit that divides the horizontal scale of the Band. The value should be one of the following unit constants (yes, Timeline has an astonishing range!):
+	- `Timeline::MILLISECOND`
+	- `Timeline::SECOND`
+	- `Timeline::MINUTE`
+	- `Timeline::HOUR`
+	- `Timeline::DAY`
+	- `Timeline::WEEK`
+	- `Timeline::MONTH`
+	- `Timeline::YEAR`
+	- `Timeline::DECADE`
+	- `Timeline::CENTURY`
+	- `Timeline::MILLENNIUM`
+- **intervalPixels**: the width of one division on the horizontal scale in pixels
+
+#### zones ####
+
+Optionally, each Timeline Band can have one or more Zones, parts where the horizontal resolution differs from the rest of the Band.
+
+`$zones` is `null`, or an array with the following keys:
+
+- **start**: beginning date of the Zone
+- **end**: ending
+- **magnify**: the multiplication factor of the resolution
+- **unit**: time unit of the zone; should be one of the unit constants
+- **multiple** (optional): modifies the horizontal scale division to multiples of the unit 
+
+## Events ##
+
+**Timeline** displays Events: Models or ActiveRecords characterized by a moment in time.
+
+The Timeline::attributes property holds the translation from Model attribute names to Timeline attribute names.
+  
+A few attributes are essential for **Timeline**. The Timeline names are:
+
+- **start**: the point in time where the Event is situated
+- **text**: the text displayed on main Timeline
+
+Events come in two types:
+
+#### Instant Events ####
+
+These are the basic Events, having just one point in time. **Timeline** displays them as little pin icons. Only the above attributes are required.
+
+#### Duration Events ####
+
+These have a certain duration. **Timeline** displays them as a piece of blue 'tape'. Apart from the above, also required is:
+
+- **end**: the point in time where, well, the Event ends.
+   
+Duration Events also have some optional attributes, making the Event *Imprecise*:
+
+- **latestStart**
+- **earliestEnd**
+
+The imprecise part of a Duration Event is displayed as faded blue tape.
+
+#### Optional Event attributes ####
+
+Some of the other Event attributes are:
+
+- **caption**: the text of the tooltip
+- **description**: the text appearing in the pop-up 'bubble'.
+- **icon**: the URL of the pin icon (only for Instant Events.
+- **color**
+- **textColor**
+- **link**: modifies the heading of the pop-up bubble to a link with this href.
+
+For the daring, there are [even more](http://simile-widgets.org/wiki/Timeline_EventSources#Additional_Event_Attributes) Event attributes. 
+
+## Dates ##
+
+**Timeline** understands a lot of date formats (in the options and in the Event data). Every date can be provided in one of the following formats:
+
+- a `string`, recognized by [Javascript Date](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date), that is in RFC2822 or ISO-8601 format; among them MySQL `date` and `datetime` fields
+- a PHP `DateTime` object
+- an `array`, recognized by Javascript Date: `[ year, month, day?, hour?, minute?, second?, millisecond? ]`. Notice: month is zero-based, so January == 0, May == 4
+- an `integer`: Unix time stamp (seconds since the Unix Epoch, 1-1-1970, return value of PHP `time()`)
---
+++ Timeline.php
@@ -0,0 +1,239 @@
+<?php
+/**
+ * MIT licence
+ * Version 1.0
+ * Sjaak Priester, Amsterdam 06-12-2014.
+ *
+ * Timeline Widget for Yii 2.0
+ *
+ * Widget integrating Simile Timeline in Yii 2.0 PHP Framework.
+ *
+ * Timeline widget renders a Simile Timeline, displaying Event data from a DataProvider.
+ *
+ * http://www.simile-widgets.org/timeline/
+ * https://code.google.com/p/simile-widgets/source/browse/#svn%2Ftimeline%2Ftags%2F2.3.1%2Fsrc%2Fwebapp%2Fapi%2Fscripts
+ *
+ */
+
+/*
+ * date parameters can be:
+ * - a string, recognized by Javascript Date, that is in RFC2822 or ISO-8601 format;
+ *      among them MySQL date and datetime fields
+ * - a PHP DateTime object
+ * - an array [ year, month, day?, hour?, minute?, second?, millisecond? ]
+ *      notice: month is zero-based, so January == 0
+ * - an integer: Unix date value (seconds since 1-1-1970)
+ */
+
+namespace sjaakp\timeline;
+
+use yii\base\Widget;
+use yii\base\InvalidConfigException;
+use yii\web\View;
+use yii\web\JsExpression;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use \DateTime;
+
+class Timeline extends Widget {
+
+    // Values for band intervalUnit; from simile/ajax/date-time.js
+    const MILLISECOND    = 0;
+    const SECOND         = 1;
+    const MINUTE         = 2;
+    const HOUR           = 3;
+    const DAY            = 4;
+    const WEEK           = 5;
+    const MONTH          = 6;
+    const YEAR           = 7;
+    const DECADE         = 8;
+    const CENTURY        = 9;
+    const MILLENNIUM     = 10;
+
+    // useful to retrieve alternative pin icons
+    const IMG_PREFIX = 'http://api.simile-widgets.org/timeline/2.3.1/images/';
+
+    /**
+     * @var \yii\data\DataProviderInterface the data provider for the timeline. This property is required.
+     */
+    public $dataProvider;
+
+    /**
+     * @var array key => value pairs of {timeline attribute name} => {model attribute name}. Required.
+     */
+    public $attributes;
+
+    /**
+     * @var int | string | false
+     * Height of the timeline.
+     * - int        height in pixels
+     * - string     valid CSS height (f.i. in ems)
+     * - false      height is not set; caution: the height MUST be set by some other means (CSS), otherwise
+     *              the timeline will not appear.
+     */
+    public $height = 200;
+
+    /**
+     * @var array
+     * HTML options of the timeline container.
+     * Use this if you want to explicitly set the ID.
+     */
+    public $htmlOptions = [];
+
+    public $timeZone;
+    
+    public $start;
+    public $end;
+    public $center;
+
+    protected $bands = [];
+    protected $syncs = [];
+
+    public function init()  {
+        if (! $this->dataProvider) {
+            throw new InvalidConfigException('The "dataProvider" property must be set.');
+        }
+        if (! $this->attributes) {
+            throw new InvalidConfigException('The "attributes" property must be set.');
+        }
+
+        if (isset($this->htmlOptions['id'])) {
+            $this->setId($this->htmlOptions['id']);
+        }
+        else $this->htmlOptions['id'] = $this->getId();
+
+        if ($this->height !== false) {
+            $style = '';
+            if (isset($this->htmlOptions['style']))
+                $style = $this->htmlOptions['style'];
+            $h = $this->height;
+            if (is_integer($h)) $h .= 'px';
+            $this->htmlOptions['style'] = $style . "height:$h;";
+        }
+
+        if (! $this->timeZone)  {
+            $dt = new DateTime();
+            $tz = $dt->getTimeZone();
+            $this->timeZone = $tz->getOffset($dt) / 3600;
+        }
+    }
+
+    public function run()   {
+        $view = $this->getView();
+        $view->registerJsFile('http://api.simile-widgets.org/timeline/2.3.1/timeline-api.js', [
+            'position' => View::POS_HEAD        // important; see: https://code.google.com/p/simile-widgets/issues/detail?id=258
+        ]);
+
+        $tData = array_map(function($model) {
+            /** @var $model \yii\base\Model */
+            $modelAtts = array_filter($model->getAttributes(array_values($this->attributes)), function($att) {
+                return ! is_null($att);
+            });
+            $v = [];
+            foreach($this->attributes as $tname => $mname)  {
+                if (isset($modelAtts[$mname])) $v[$tname] = $modelAtts[$mname];
+            }
+            $v = $this->handleDates($v, ['start', 'end', 'latestStart', 'earliestEnd']);
+            if (isset($v['end'])) $v['durationEvent'] = true;
+            $jv = Json::encode($v);
+            return new JsExpression("new Timeline.DefaultEventSource.Event($jv)");
+        }, $this->dataProvider->getModels());
+
+        $jData = Json::encode($tData);
+
+        $id = $this->getId();
+
+        $jSyncs = Json::encode($this->syncs);
+        $jBands = Json::encode($this->bands);
+
+        $js = "var {$id}m=Timeline.ClassicTheme.create(),{$id}s=new Timeline.DefaultEventSource();{$id}s.addMany($jData);";
+
+        if ($this->start)   {
+            $start = $this->dateToJs($this->start);
+            $js .= "{$id}m.timeline_start=$start;";
+        }
+
+        if ($this->end)   {
+            $end = $this->dateToJs($this->end);
+            $js .= "{$id}m.timeline_stop=$end;";
+        }
+
+        $js .= "var {$id}r=null,{$id}=Timeline.create(document.getElementById('$id'),jQuery.extend(true,$jBands,$jSyncs)).finishedEventLoading();
+jQuery(window).resize(function(){if(!{$id}r){{$id}r=setTimeout(function(){{$id}r=null;{$id}.layout();},500);}});";
+
+        $view->registerJs($js);
+
+        echo Html::tag('div', '', $this->htmlOptions);
+    }
+
+    /**
+     * @param $options array
+     * - width
+     * - layout     the only sensible value is 'overview'; 'detailed' will also work, but seems to be very buggy
+     *              all other values default to 'compact'
+     * - intervalUnit
+     * - intervalPixels
+     * - syncWith   zero-based index of band to sync this band to, or false
+     *              default: the index of the previous band
+     * - highlight
+     * - timeZone   if not set, will be set to $this->timeZone
+     * @return $this
+     */
+    public function band($options, $zones = null)  {
+        $sync = [];
+        $create = '';
+        if (is_array($options)) {
+            if (count($this->bands))    {
+                if (isset($options['syncWith']))    {
+                    if ($options['syncWith'] !== false) $sync['syncWith'] = $options['syncWith'];
+                    unset($options['syncWith']);
+                }
+                else $sync['syncWith'] = count($this->bands) - 1;
+                if (isset($options['highlight']))    {
+                    $sync['highlight'] = $options['highlight'];
+                    unset($options['highlight']);
+                }
+                else $sync['highlight'] = true;
+            }
+            else $sync = new JsExpression('{}');
+            if (!array_key_exists('eventSource', $options)) {
+                $options['eventSource'] = new JsExpression($this->getId() . 's');
+            }
+            if (!array_key_exists('theme', $options)) {
+                $options['theme'] = new JsExpression($this->getId() . 'm');
+            }
+            if ($this->center) $options['date'] = $this->dateToJs($this->center);
+            if (! isset($options['timeZone']) && $this->timeZone) $options['timeZone'] = $this->timeZone;
+            if (is_array($zones)) {
+                $create = 'HotZone';
+                $options['zones'] = array_map([$this, 'handleDates'], $zones);
+            }
+        }
+        $this->syncs[] = $sync;
+
+        $opts = Json::encode($options);
+        $this->bands[] = new JsExpression("Timeline.create{$create}BandInfo($opts)");
+
+        return $this;
+    }
+
+    protected function handleDates($v, $attNames = ['start', 'end']) {
+        foreach($attNames as $dateAttr)  {
+            if (array_key_exists($dateAttr, $v))    {
+                $v[$dateAttr] = $this->dateToJs($v[$dateAttr]);
+            }
+        }
+        return $v;
+    }
+    
+    protected function dateToJs($val) {
+        if (is_a($val, 'DateTime')) {
+            /** @var $val DateTime */
+            $val = $val->format('U') . '000';  // Javascript Date has value in milliseconds
+        }
+        else if (is_array($val)) $val = implode(',', $val);
+        else if (is_numeric($val)) $val .= '000';
+        else if (is_string($val)) $val = "'$val'";
+        return new JsExpression("new Date($val)");;
+    }
+}
